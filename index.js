304b1bbcd87d7c5ca51a1895abbc8a90186c3f34
pravinpoudel <prvnpoudel4@gmail.com>
2021-02-20 11:09:09 +0545

    router is added to project, main page opened with sendFile

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const router = express.Router();
+
+let port = process.env.port || 3099;
+
+app.use(router);
+
+router.get("/", (req, res)=>{
+    console.log("hello world");
+    res.sendFile('index.html', {root: path.join( __dirname, '/public/')})
+});
 
 
-app.listen(3000, function(){})
+app.use(express.static('public', {index: false}));
+
+app.listen(port, (err)=> {
+    if(err){
+        return(console.log("i am listening"));
+    }
+    console.log(`listening to the port ${port}`);
+});
