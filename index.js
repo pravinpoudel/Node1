79e777c03265e39d6df04f27ee49633cc4359b76
pravinpoudel <prvnpoudel4@gmail.com>
2021-02-20 11:35:45 +0545

    comment added to the code

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
+"use strict";
 const express = require("express");
 const path = require("path");
 
+// here we create an object called app that is gonna be our webserver
 const app = express();
 const router = express.Router();
 
-let port = process.env.port || 3099;
+let port = process.env.port || 3000;
 
 app.use(router);
 
 router.get("/", (req, res)=>{
     console.log("hello world");
-    res.sendFile('index.html', {root: path.join( __dirname, '/public/')})
+
+    // sendfile demand absolute path so for that we have option with root
+    res.sendFile('index.html', {root: path.join( __dirname, '/public/')});
+    //note that we are not providing absolute path by our own or with __dirname concatinated with public 
+    // because this may conflict with the convention of front slash and back slash so here path.join() come handy
+
 });
 
-
+// here since we have option with false indexing mean that it wont take index.html as a default file for 404 in static serve also
 app.use(express.static('public', {index: false}));
 
 app.listen(port, (err)=> {
